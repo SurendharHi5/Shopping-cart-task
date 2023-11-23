0d82fe2f962693aab3210f1ce1d828b323b8ae16
Surendhar C <142418478+SurendharHi5@users.noreply.github.com>
2023-11-22 19:03:17 -0800

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="py-5 bg-dark">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright &copy; Your Website 2021
+            Copyright &copy; Your Website 2023
           </p>
         </div>
       </footer>
